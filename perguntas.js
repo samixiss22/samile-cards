bfa79fd735a381241122c00325a52587b0911c6b
samixiss22 <samile.oliveira@escola.pr.gov.br>
2024-10-08 08:48:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'desenhos',  
+    'qual a cor do vestido da Monica?',
+    'o vestido da Monica é vermelho'
+)
+
+criaCartao(
+    'desenhos',
+    'onde mora o bob esponja?',
+    'em um abacaxi no mar'
+)
+
+criaCartao(
+    'desenhos',
+    'qual nome da princesa que perde o sapatinho?',
+    'a princesa é a Cinderela'
+)
+
+criaCartao(
+    'desenhos',
+    'qual o trabalho de ladybug e catnoir?',
+    'salvar a cidade dos perigos'
+)
+
+criaCartao(
+    'desenhos',
+    'qual o nome da esquilo de bob esponja',
+    'o nome é Sandy'
+)
+
+criaCartao(
+    'desenhos',
+    'qual a cor do vestido da bela de a bela e a fera',
+    'o vestido é amarelo'
+)
